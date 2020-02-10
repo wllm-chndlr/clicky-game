eb278493d84754ed455db8daaff187c08eee50d8
William Chandler <chndlr.wllm@gmail.com>
2020-02-09 21:18:49 -0600

    removed unused imports

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Nav from "./components/Nav";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import Container from "./Container";
-import Row from "./Row";
-import Column from "./Column";
 import friends from "./friends.json";
 import "./App.css";
 import Message from "./components/Message";
